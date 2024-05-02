e67ecadc03efb3db12f4502303a47ffb278ac2b2
yyt331 <124370512+yyt331@users.noreply.github.com>
2024-05-02 10:09:03 +0100

    Update create_csv.py

--- Backend/preprocessing/create_csv.py
+++ Backend/preprocessing/create_csv.py
@@ -6,21 +6,19 @@ def create_csv_from_folders(normal_folder, abnormal_folder, csv_file):
         writer = csv.writer(file)
         writer.writerow(['image_path', 'label'])
 
-        # Write the paths and labels for normal images
         for filename in os.listdir(normal_folder):
             if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
                 img_path = os.path.join(normal_folder, filename).replace('\\', '/')
-                writer.writerow([img_path, 0])  # Label 0 for normal
+                writer.writerow([img_path, 0])
 
-        # Write the paths and labels for abnormal images
         for filename in os.listdir(abnormal_folder):
             if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
                 img_path = os.path.join(abnormal_folder, filename).replace('\\', '/')
-                writer.writerow([img_path, 1])  # Label 1 for abnormal
+                writer.writerow([img_path, 1])
 
-# Usage
+# replace the file paths with the actual file paths 
 normal_folder_path = '/Users/Windows/Desktop/test_normal'
 abnormal_folder_path = '/Users/Windows/Desktop/test_abnormal'
-csv_file_path = '/Users/Windows/Desktop/test_labelled_dataset.csv'
+csv_file_path = '/Users/Windows/Desktop/labelled_dataset.csv'
 
 create_csv_from_folders(normal_folder_path, abnormal_folder_path, csv_file_path)
